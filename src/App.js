735166c31cff5dfb7ef10f614b4d421029494fa2
bel1c10ud <mail@bellclou.de>
2025-10-28 18:08:35 +0900

    feat: 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 class App {
-  async run() {}
+  async getBudgetAsync() {
+    try {
+      const budget = await Console.readLineAsync("구입금액을 입력해 주세요.");
+      return budget;
+    } catch (error) {
+      throw new Error("[ERROR] 구입 금액을 입력받던 중 오류가 발생했습니다.");
+    }
+  }
+
+  async getWinningNumbersAsync() {
+    try {
+      const winningNumbers = await Console.readLineAsync("당첨 번호를 입력해 주세요.");
+      return winningNumbers;
+    } catch (error) {
+      throw new Error("[ERROR] 당첨 번호를 입력받던 중 오류가 발생했습니다.");
+    }
+  }
+
+  async getBonusNumberAsync() {
+    try {
+      const bonusNumber = await Console.readLineAsync("보너스 번호를 입력해 주세요.");
+      return bonusNumber;
+    } catch (error) {
+      throw new Error("[ERROR] 보너스 번호를 입력받던 중 오류가 발생했습니다.");
+    }
+  }
+
+  async run() {
+    const budgetStr = await this.getBudgetAsync();
+    const winningNumbersStr = await this.getWinningNumbersAsync();
+    const bonusNumbersStr = await this.getBonusNumberAsync();
+  }
 }
 
 export default App;
