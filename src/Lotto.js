85803de869974d508dd503ace8ca69ed26a93559
bel1c10ud <mail@bellclou.de>
2025-10-29 12:34:48 +0900

    feat: 로또 번호에 대한 예외 처리 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -12,6 +12,14 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+
+    if (numbers.some((number) => isNaN(number) || number < 1 || number > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+    }
   }
 
   print() {
